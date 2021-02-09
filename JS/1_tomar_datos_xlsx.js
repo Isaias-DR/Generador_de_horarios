14d7dc1dd8fa3097efebccad0bbe913e92d52865
Isaias-DR <78326629+Isaias-DR@users.noreply.github.com>
2021-02-09 20:41:49 -0300

    Revisando toma de datos

--- JS/1_tomar_datos_xlsx.js
+++ JS/1_tomar_datos_xlsx.js
@@ -8,10 +8,13 @@ var ExcelToJSON = function() {
 
 		reader.onload = function(e) {
 
+			var XL_row_object, json_object, json_object_parse;
+
 			var data = e.target.result;
-			var workbook = XLSX.read(data, {
-				type: 'binary'
-			});
+			var workbook = XLSX.read(
+				data, 
+				{ type: 'binary' }
+			);
 
 			// Recorrer las hojas del Excel
 			workbook.SheetNames.forEach(function(sheetName) {
@@ -19,19 +22,22 @@ var ExcelToJSON = function() {
 				// Guarda los nombres de las hojas
 				json_hojas.push(sheetName);
 
-				// Guarda los datos de esa hoha
-				var XL_row_object = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheetName]);
+				// Guarda los datos de esa hoja
+				XL_row_object = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheetName]);
 				
-				var json_object = JSON.stringify(XL_row_object); // stringify combierte un objeto o valor a una cadena de texto
-				var json_object_parse = JSON.parse(json_object); // analiza una cadena de texto como JSON, transformando opcionalmente  el valor producido por el análisis.
+				json_object = JSON.stringify(XL_row_object); // stringify combierte un objeto o valor a una cadena de texto
+				json_object_parse = JSON.parse(json_object); // analiza una cadena de texto como JSON, transformando opcionalmente  el valor producido por el análisis.
 				console.log(json_object_parse);
 				jQuery( '#xlx_json' ).val( json_object );
-				//jQuery( '#xlx_json' ).val( XL_row_object );
-			
 
-				// Listar asignaturas
-				const distinctAsignaturas = [...new Set(json_object_parse.map(x => x.Asignatura))]
-				//jQuery( '#asignaturas' ).val( json_asignaturas );               
+				if(sheetName == "Hoja1"){
+					// Listar asignaturas
+					const distinctAsignaturas = [ ...new Set(json_object_parse.map(x => x.Asignatura)) ];
+					console.log(distinctAsignaturas);
+					console.log(distinctAsignaturas[2]);
+					jQuery( '#asignaturas' ).val( distinctAsignaturas );  
+				}
+
 			})
 
 			// Listar las hojas del Excel
