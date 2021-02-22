74fe062f1bd7961e4c43b1a7b6705377b1f9c21c
Isaias-DR <78326629+Isaias-DR@users.noreply.github.com>
2021-02-22 15:05:24 -0300

    Avance parte 3

--- tree/main/JS/uso_de_xlsx.js
+++ tree/main/JS/uso_de_xlsx.js
@@ -1,9 +1,8 @@
-//let json_leaves = [];
-let array_disabled = [];
+let arr_columns_name = ["Seleccione...", "Año", "Período", "Sede", "Escuela", "Carrera", "Plan", "Tipo Plan de Estudios", "Jornada", "Tipo Asignatura", "Sigla", "Asignatura", "Sección unificado", "Sección duplicado", "Créditos", "Horario unificado", "Horario duplicado", "Sala", "Docente", "Día"];
 var XL_row_object, json_object, json_object_parse, workbook;
 
 function handleFileSelect(evt) {
-	
+
 	var files = evt.target.files; // Lista de objetos de archivos
 	var xl2json = new ExcelToJSON();
 	xl2json.parseExcel(files[0]);
@@ -18,26 +17,20 @@ class ExcelToJSON {
 			reader.onload = function (e) {
 
 				var data = e.target.result;
-				workbook = XLSX.read( data, { type: 'binary' } );
+				workbook = XLSX.read(data, { type: 'binary' });
 
 				// Recorrer las hojas del Excel
 				workbook.SheetNames.forEach(function (sheetName) {
 
-					// Guarda los nombres de las hojas
-					//json_leaves.push(sheetName);
-
 					// Crear HTML del los radios
 					document.getElementById("div_leaves").innerHTML = document.getElementById("div_leaves").innerHTML +
-					"<input class='form-check-input' type='radio' id='hojas' name='hojas' value='"+sheetName+"' onchange='handleChange(event);' >" +
-					"<label class='form-check-label' for="+sheetName+">"+ sheetName+"</label><br>";
+						"<input class='form-check-input' type='radio' id='hojas' name='hojas' value='" + sheetName + "' onchange='handleChange(event);' >" +
+						"<label class='form-check-label' for=" + sheetName + ">" + sheetName + "</label><br>";
 				});
-
-				// Listar las hojas del Excel
-				//console.log(json_leaves);
-				//jQuery('#hojas').val(json_leaves); // No esta en HTML el elelmento #hojas
 			};
 
 			reader.onerror = function (ex) {
+
 				console.log(ex);
 			};
 
@@ -46,7 +39,7 @@ class ExcelToJSON {
 	}
 }
 
-function handleChange(e){
+function handleChange(e) {
 
 	//console.log("Valor elegido " + e.currentTarget.value);	
 
@@ -59,24 +52,28 @@ function handleChange(e){
 	console.log(json_object_parse);
 	//jQuery('#xlx_json').val(json_object_parse); // El json_object se demora paro cuendo esta adentro del forEach ahi no \(º_º)/
 
-	selectNames();
-}
-
-function selectNames(){
 
 	// Saber los nombres de las columnas
-	let columns_name = Object.getOwnPropertyNames(json_object_parse[0]);
+	let columns_name = Object.getOwnPropertyNames(json_object_parse[5]);
 	//console.log(Object.getOwnPropertyNames(json_object_parse[0]));
+
+	var string_selection2 = "";
+	for (let l = 0; l < arr_columns_name.length; l++) {
+		var name = arr_columns_name[l];
+		//console.log(name);
+		//console.log(l);
+		string_selection2 = string_selection2 + "<option value='" + l + "'>" + name + "</option>";
+	}
 
 	// Dibujar
 	for (let k = 0; k < columns_name.length; k++) {
 		const columnName = columns_name[k];
 		document.getElementById("div_columns_name").innerHTML = document.getElementById("div_columns_name").innerHTML +
-		"<p>"+columnName+"</p>";
+			"<p>" + columnName + "<select name='selectName' id='selectName" + k + "' class='custom-select custom-select-lg mb-3'>" + string_selection2 + "</select>" + "</p>";
 	}
 }
 
-function filter(){
+function filter() {
 
 	/* Listar asignaturas
 	var distinctAsignaturas = [...new Set(json_object_parse.map(x => x.Asignatura))];
@@ -87,7 +84,7 @@ function filter(){
 
 }
 
-function drawTableWhitData(){
+function drawTableWhitData() {
 	/*
 	document.getElementById("div_data").innerHTML = document.getElementById("div_data").innerHTML +
 	<table class='table table-striped'>
