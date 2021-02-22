c6585f1cac2049216785db53df02a27565cf3923
Isaias-DR <78326629+Isaias-DR@users.noreply.github.com>
2021-02-21 21:33:04 -0300

    Avance parte 3
    Lista los nombre de las columnas

--- tree/main/JS/uso_de_xlsx.js
+++ tree/main/JS/uso_de_xlsx.js
@@ -1,6 +1,13 @@
-let json_hojas = [];
-var indexElegido;
+//let json_leaves = [];
+let array_disabled = [];
 var XL_row_object, json_object, json_object_parse, workbook;
+
+function handleFileSelect(evt) {
+	
+	var files = evt.target.files; // Lista de objetos de archivos
+	var xl2json = new ExcelToJSON();
+	xl2json.parseExcel(files[0]);
+}
 
 class ExcelToJSON {
 	constructor() {
@@ -11,25 +18,23 @@ class ExcelToJSON {
 			reader.onload = function (e) {
 
 				var data = e.target.result;
-				workbook = XLSX.read(
-					data,
-					{ type: 'binary' }
-				);
+				workbook = XLSX.read( data, { type: 'binary' } );
 
 				// Recorrer las hojas del Excel
 				workbook.SheetNames.forEach(function (sheetName) {
 
 					// Guarda los nombres de las hojas
-					json_hojas.push(sheetName);
+					//json_leaves.push(sheetName);
 
 					// Crear HTML del los radios
-					
-					document.getElementById("div_hojas").innerHTML = document.getElementById("div_hojas").innerHTML + "<input class='form-check-input' type='radio' id='hojas' name='hojas' value='"+sheetName+"' onchange='handleChange(event);' > <label class='form-check-label' for="+sheetName+">"+ sheetName+"</label><br>";
+					document.getElementById("div_leaves").innerHTML = document.getElementById("div_leaves").innerHTML +
+					"<input class='form-check-input' type='radio' id='hojas' name='hojas' value='"+sheetName+"' onchange='handleChange(event);' >" +
+					"<label class='form-check-label' for="+sheetName+">"+ sheetName+"</label><br>";
 				});
-				/* Listar las hojas del Excel
-				*/
-				console.log(json_hojas);
-				//jQuery('#hojas').val(json_hojas);
+
+				// Listar las hojas del Excel
+				//console.log(json_leaves);
+				//jQuery('#hojas').val(json_leaves); // No esta en HTML el elelmento #hojas
 			};
 
 			reader.onerror = function (ex) {
@@ -39,13 +44,13 @@ class ExcelToJSON {
 			reader.readAsBinaryString(file);
 		};
 	}
-}	
+}
 
 function handleChange(e){
 
 	//console.log("Valor elegido " + e.currentTarget.value);	
 
-	/* Tomar y guarda los datos de esa hoja elegida                       */
+	/* Tomar y guarda los datos de esa hoja elegida*/
 	XL_row_object = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[e.currentTarget.value]); // objeto
 	json_object = JSON.stringify(XL_row_object); // texto - stringify combierte un objeto o valor a una cadena de texto
 	json_object_parse = JSON.parse(json_object); // JSON - analiza una cadena de texto como JSON, transformando opcionalmente  el valor producido por el análisis.
@@ -53,9 +58,25 @@ function handleChange(e){
 	/* Listar contenido*/
 	console.log(json_object_parse);
 	//jQuery('#xlx_json').val(json_object_parse); // El json_object se demora paro cuendo esta adentro del forEach ahi no \(º_º)/
+
+	selectNames();
 }
 
-function filtar(){	
+function selectNames(){
+
+	// Saber los nombres de las columnas
+	let columns_name = Object.getOwnPropertyNames(json_object_parse[0]);
+	//console.log(Object.getOwnPropertyNames(json_object_parse[0]));
+
+	// Dibujar
+	for (let k = 0; k < columns_name.length; k++) {
+		const columnName = columns_name[k];
+		document.getElementById("div_columns_name").innerHTML = document.getElementById("div_columns_name").innerHTML +
+		"<p>"+columnName+"</p>";
+	}
+}
+
+function filter(){
 
 	/* Listar asignaturas
 	var distinctAsignaturas = [...new Set(json_object_parse.map(x => x.Asignatura))];
@@ -66,9 +87,39 @@ function filtar(){
 
 }
 
-function handleFileSelect(evt) {
-	
-	var files = evt.target.files; // Lista de objetos de archivos
-	var xl2json = new ExcelToJSON();
-	xl2json.parseExcel(files[0]);
+function drawTableWhitData(){
+	/*
+	document.getElementById("div_data").innerHTML = document.getElementById("div_data").innerHTML +
+	<table class='table table-striped'>
+		<caption>Contenido</caption>
+		<thead>
+			<tr>
+				<th scope='col'>#</th>
+				<th scope='col'>First</th>
+				<th scope='col'>Last</th>
+				<th scope='col'>Handle</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<th scope='row'>1</th>
+				<td>Mark</td>
+				<td>Otto</td>
+				<td>@mdo</td>
+			</tr>
+			<tr>
+				<th scope='row'>2</th>
+				<td>Jacob</td>
+				<td>Thornton</td>
+				<td>@fat</td>
+			</tr>
+			<tr>
+				<th scope='row'>3</th>
+				<td>Larry</td>
+				<td>the Bird</td>
+				<td>@twitter</td>
+			</tr>
+		</tbody>
+	</table>";
+	*/
 }
