31b7332059f3b277e14dbb4df511493163fc0195
Isaias-DR <78326629+Isaias-DR@users.noreply.github.com>
2021-03-01 20:47:57 -0300

    Interpretando selección, parte 3

--- tree/main/JS/uso_de_xlsx.js
+++ tree/main/JS/uso_de_xlsx.js
@@ -1,5 +1,6 @@
 let arr_columns_name = ["Seleccione...", "Año", "Período", "Sede", "Escuela", "Carrera", "Plan", "Tipo Plan de Estudios", "Jornada", "Tipo Asignatura", "Sigla", "Asignatura", "Sección unificado", "Sección duplicado", "Créditos", "Horario unificado", "Horario duplicado", "Sala", "Docente", "Día"];
-var XL_row_object, json_object, json_object_parse, workbook;
+let columns_name, columns_name_selected = [];
+var XL_row_object, json_object, json_object_parse, workbook, index_name_subjet, distinctAsignaturas;
 
 function handleFileSelect(evt) {
 
@@ -25,7 +26,7 @@ class ExcelToJSON {
 					// Crear HTML del los radios
 					document.getElementById("div_leaves").innerHTML = document.getElementById("div_leaves").innerHTML +
 						"<input class='form-check-input' type='radio' id='hojas' name='hojas' value='" + sheetName + "' onchange='handleChange(event);' >" +
-						"<label class='form-check-label' for=" + sheetName + ">" + sheetName + "</label><br>";
+						"<label class='form-check-label' for='" + sheetName + "'>" + sheetName + "</label><br>";
 				});
 			};
 
@@ -41,50 +42,69 @@ class ExcelToJSON {
 
 function handleChange(e) {
 
-	//console.log("Valor elegido " + e.currentTarget.value);	
-
-	/* Tomar y guarda los datos de esa hoja elegida*/
+	// Tomar y guarda todos los datos de esa hoja elegida
 	XL_row_object = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[e.currentTarget.value]); // objeto
 	json_object = JSON.stringify(XL_row_object); // texto - stringify combierte un objeto o valor a una cadena de texto
 	json_object_parse = JSON.parse(json_object); // JSON - analiza una cadena de texto como JSON, transformando opcionalmente  el valor producido por el análisis.
 
-	/* Listar contenido*/
 	console.log(json_object_parse);
-	//jQuery('#xlx_json').val(json_object_parse); // El json_object se demora paro cuendo esta adentro del forEach ahi no \(º_º)/
 
 
+	// Tomar y muestra los nombres de cada nombre de columna
 	// Saber los nombres de las columnas
-	let columns_name = Object.getOwnPropertyNames(json_object_parse[5]);
-	//console.log(Object.getOwnPropertyNames(json_object_parse[0]));
+	columns_name = Object.getOwnPropertyNames(json_object_parse[0]);
 
-	var string_selection2 = "";
-	for (let l = 0; l < arr_columns_name.length; l++) {
-		var name = arr_columns_name[l];
-		//console.log(name);
-		//console.log(l);
-		string_selection2 = string_selection2 + "<option value='" + l + "'>" + name + "</option>";
+	var string_selection = "";
+	for (let k = 0; k < arr_columns_name.length; k++) {
+
+		var name = arr_columns_name[k];
+		string_selection = string_selection + "<option value='" + k + "'>" + name + "</option>";
 	}
 
 	// Dibujar
-	for (let k = 0; k < columns_name.length; k++) {
-		const columnName = columns_name[k];
+	for (let l = 0; l < columns_name.length; l++) {
+
+		const columnName = columns_name[l];
 		document.getElementById("div_columns_name").innerHTML = document.getElementById("div_columns_name").innerHTML +
-			"<p>" + columnName + "<select name='selectName' id='selectName" + k + "' class='custom-select custom-select-lg mb-3'>" + string_selection2 + "</select>" + "</p>";
+			"<p>" + columnName + "<select name='selectName' id='selectName" + l + "' class='custom-select custom-select-lg mb-3'>" + string_selection + "</select>" + "</p>";
 	}
+
+	// Agregar boton
+	document.getElementById("div_columns_name").innerHTML = document.getElementById("div_columns_name").innerHTML +
+	"<button onclick='getValues()'>Calcular</button>";
 }
 
-function filter() {
+function getValues() {
 
-	/* Listar asignaturas
-	var distinctAsignaturas = [...new Set(json_object_parse.map(x => x.Asignatura))];
+	// Guardar referencia a las columnas
+	columns_name_selected = [];
+	for (let ll = 0; ll < columns_name.length; ll++) {
+		
+		columns_name_selected.push(document.getElementById("selectName"+ll).value);
+	}
+	console.log(columns_name_selected);
+	console.log(columns_name);
+
+	showSubject();
+}
+
+function showSubject() {
+
+	// Saber el index elegido de la asignatura
+	console.log(columns_name_selected.indexOf("11"));
+	index_name_subjet = columns_name_selected.indexOf("11");
+	// Listar asignaturas
+	console.log(columns_name[index_name_subjet]);
+	if (index_name_subjet == "11") {
+		
+		distinctAsignaturas = [...new Set(json_object_parse.map(x => x.Asignatura))];
+	}
 	console.log(distinctAsignaturas);
 	console.log(distinctAsignaturas[2]);
-	jQuery('#asignaturas').val(distinctAsignaturas);
-	*/
-
 }
 
 function drawTableWhitData() {
+
 	/*
 	document.getElementById("div_data").innerHTML = document.getElementById("div_data").innerHTML +
 	<table class='table table-striped'>
